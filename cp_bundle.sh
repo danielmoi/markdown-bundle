64268dc7e96307f6b516e2673eab40678e5f63b1
danielmoi <dmoixy@gmail.com>
2019-09-06 05:52:58 +1000

    Copy for web assets too

--- cp_bundle.sh
+++ cp_bundle.sh
@@ -6,6 +6,7 @@ echo $MARKDOWN_READER_MARKDOWN_ASSETS_BUNDLE_PATH
 # -v        Verbose
 # --delete  Remove extraneous files from dest dirs
 
+# iOS
 rsync -rv \
   --exclude="cp_bundle.sh" \
   --exclude=".git" \
@@ -13,3 +14,22 @@ rsync -rv \
   --delete \
   $MARKDOWN_ASSETS_PATH \
   $MARKDOWN_READER_MARKDOWN_ASSETS_BUNDLE_PATH
+
+# Web
+rsync -rv \
+  --delete \
+  $MARKDOWN_ASSETS_PATH/css/ \
+  $FANZCA_ZIP_PATH/css/
+
+rsync -rv \
+  --delete \
+  $MARKDOWN_ASSETS_PATH/js/ \
+  $FANZCA_ZIP_PATH/js/
+
+cd $FANZCA_ZIP_PATH/
+CSS_WEB_ZIP_NAME="css.zip"
+zip -r $CSS_WEB_ZIP_NAME css
+
+cd $FANZCA_ZIP_PATH/
+JS_WEB_ZIP_NAME="js.zip"
+zip -r $JS_WEB_ZIP_NAME js
